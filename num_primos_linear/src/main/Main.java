d14a2196f675e3b4f387680e8c82015672903cf4
lorenzoeid <lorenzocolaferro12@gmail.com>
2025-03-13 13:35:08 -0300

    Corrigindo a saída e adicionando o 11

--- num_primos_linear/src/main/Main.java
+++ num_primos_linear/src/main/Main.java
@@ -17,8 +17,8 @@ public class Main {
 				System.out.println("Números primos: 2");
 			}else {
 				for (int x = 2; x < n; x++) {
-					if ((x % 2 != 0) && (x % 3 != 0) && (x % 5 != 0) && (x % 7 != 0) && (x % 9 != 0) || (x == 2) | (x == 3) | (x == 5) | (x == 7)){
-							System.out.println(x + " ");
+					if ((x % 2 != 0) && (x % 3 != 0) && (x % 5 != 0) && (x % 7 != 0) && (x % 11 != 0) || (x == 2) | (x == 3) | (x == 5) | (x == 7) | (x == 11)){
+							System.out.println("Números primos: " + x + " ");
 						}
 			}
 		}
